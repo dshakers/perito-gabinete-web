0e1cd7b7a7fe230c1ede206d759020754ed0888c
Dani <daniellianes23@gmail.com>
2026-02-11 15:07:45 +0000

    Fix: Add tailwind config, clean project structure and fix GitHub Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind({
+    applyBaseStyles: true,
+  })],
   site: 'https://dshakers.github.io',
   base: '/perito-gabinete-web',
+  output: 'static',
 });
